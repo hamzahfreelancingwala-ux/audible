50dbb846608998f70eb5e1f72c05d53a7643710a
hamzahfreelancingwala-ux <hamzahfreelancingwala@gmail.com>
2026-03-05 06:02:48 +0400

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'rsoa_rsoa112_59';
+$username = 'rsoa_rsoa112_59';
+$password = '123456';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
